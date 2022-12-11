fdcd5e9e2ef5430cbe55c75b303b57bce9339a1a
DorofD <Legion.0@yandex.ru>
2022-12-11 19:00:07 +0300

    добавление постов

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
 
-import sqlite3, os
+import sqlite3, os, math, time
 
 # конфигурация БД
 
@@ -27,7 +27,33 @@ def create_db():
     db.commit()
     db.close()
 
-# app.config['SECRET_KEY'] = 'amogus1488'
+class FDataBase:
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def getMenu(self):
+        sql = '''SELECT * FROM mainmenu'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            print(res)
+            if res: 
+                return res
+        except:
+            print('Ошибка чтения из БД')
+        return []
+
+    def addPost(self, title, text):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute('INSERT INTO posts VALUES(NULL, ?, ?, ?)', (title, text, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print('Ошибка добавления статьи в БД: ' + str(e))
+            return False
+        return True
+
 
 menu = [{'name':'Установка', 'url': 'install-flask'},
         {'name':'Первое приложение', 'url': 'first-app'},
@@ -41,8 +67,8 @@ def get_db():
 @app.route('/')
 def index():
     db = get_db()
-    print(url_for('index'))
-    return render_template('index.html', title = 'Главная страница', menu = menu)
+    dbase = FDataBase(db)
+    return render_template('index.html', title = 'Главная страница', menu = dbase.getMenu())
 
 @app.teardown_appcontext
 def close_db(error):
@@ -51,14 +77,12 @@ def close_db(error):
 
 @app.route('/about')
 def about():
-    print(url_for('about'))
     return render_template('about.html', title = 'О сайте', menu = menu)
 
 @app.route('/profile/<username>')
 def profile(username):
     if 'userLogged' not in session or session['userLogged'] != username:
         abort(401)
-        print(session)
     return f'Пользователь: {username}'
 
 @app.route('/contact', methods=['POST', 'GET'])
@@ -79,6 +103,24 @@ def login():
         session['userLogged'] = request.form['username']
         return redirect(url_for('profile', username=session['userLogged']))
     return render_template('login.html', title='Авторизация', menu=menu)
+
+@app.route('/add_post', methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category = 'error')
+            else:
+                flash('Статья успешно добавлена', category='success')
+        else:
+            flash('Ошибка добавления статьи', category = 'error')
+    
+    return render_template('add_post.html', title = 'Добавление статьи', menu = dbase.getMenu())
+
 
 @app.errorhandler(404)
 def pageNotFound(error):
