f19a77c8b8809ea16ab661a29a5449baecdad173
DorofD <Legion.0@yandex.ru>
2022-12-12 22:53:28 +0300

    формирование urlов картинок при добавлении статьи

--- app.py
+++ app.py
@@ -50,6 +50,10 @@ class FDataBase:
             if res['count'] > 0:
                 print('Статья уже существует')
                 return False
+            base = url_for('static', filename='images_html')
+            text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                "\\g<tag>" + base + "/\\g<url>>",
+                text)
 
             tm = math.floor(time.time())
             self.__cur.execute('INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)', (title, text, url, tm))
@@ -64,11 +68,7 @@ class FDataBase:
             self.__cur.execute(f"SELECT title, text FROM posts WHERE url LIKE '{alias}' LIMIT 1")
             res = self.__cur.fetchone()
             if res:
-                base = url_for('static', filename='images_html')
-                text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
-                "\\g<tag>" + base + "/\\g<url>>",
-                res['text'])
-                return (res['title'], text)
+                return res
         except sqlite3.Error as e:
             print('Ошибка добавления статьи в БД: ' + str(e))
         return(False, False)
